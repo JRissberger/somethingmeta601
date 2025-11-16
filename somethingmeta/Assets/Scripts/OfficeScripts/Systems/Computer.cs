988cd10dc8e7add5f414cb7ba53c850e6f6c152d
Josh Chen <143774360+BlueAur@users.noreply.github.com>
2025-11-16 17:48:32 -0500

    Added Code to Altar and Labyrinth

--- somethingmeta/Assets/Scripts/OfficeScripts/Systems/Computer.cs
+++ somethingmeta/Assets/Scripts/OfficeScripts/Systems/Computer.cs
@@ -29,7 +29,8 @@ public class Computer : MonoBehaviour
         {"TY4P", "Credits"},
         {"SB7YVT8", "BeastHunter"},
         {"S3HUN9E", "PicnicScene"},
-        {"LAB", "Labyrinth"}
+        {"SC0NNIE", "Labyrinth"},
+        {"ST5L313", "AltarPuzzle"}
     };
 
     private void Start()
